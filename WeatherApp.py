7237e44efa9445aca36bebcfaa3d44013dc354e5
devpool007 <sharmadevansh007777@gmail.com>
2019-03-11 13:47:32 +0530

    test1

--- WeatherApp.py
+++ WeatherApp.py
@@ -5,25 +5,24 @@ HEIGHT = 800
 WIDTH = 800
 root = tk.Tk()
 root.title("XXX  PEACE KEEPER  XXX")
-photo = tk.PhotoImage(file="camo.PPM")
+#photo = tk.PhotoImage(file="camo.PPM")
 
-l = tk.Label(root, image=photo)
-l.pack()
+# l = tk.Label(root, image=photo)
+# l.pack()
+# label = tk.Label(frame,text="This is Label",bg="yellow")
+# label.place(relx=0.3,rely=0,relwidth=0.45,relheight=0.25)
 
 canvas = tk.Canvas(root,height=HEIGHT,width=WIDTH)
 canvas.pack()
 
+frame = tk.Frame(root,bg="#80c1ff")
+frame.place(relx=0.5,rely=0.1,relwidth=0.75,relheight=0.1,anchor="n")
 
-frame = tk.Frame(root,bg="#7B857B")
-frame.place(relx=0.1,rely=0.1,relwidth=0.8,relheight=0.8)
+entry = tk.Entry(frame ,font=40)
+entry.place(relwidth=0.65,relheight=1)
 
-button = tk.Button(frame, text = "  ON  ", bg="red", fg="green")#highlightbackground="#7B857B")
-button.grid()
+button = tk.Button(frame, text = "  ON  ", bg="red", fg="green",highlightbackground="#80c1ff",font=40)
+button.place(relx=0.7,relwidth=0.35,relheight=1)
 
-label = tk.Label(frame,text="This is Label",bg="yellow")
-label.grid()
-
-entry = tk.Entry(frame,bg="green")
-entry.grid()
 
 root.mainloop()
