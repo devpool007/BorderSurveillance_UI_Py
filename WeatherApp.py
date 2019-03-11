801daf06b7f048dbbabcb77138c475b0ce827a38
devpool007 <sharmadevansh007777@gmail.com>
2019-03-11 17:15:18 +0530

    first part

--- WeatherApp.py
+++ WeatherApp.py
@@ -1,28 +1,48 @@
 #!/usr/bin/python3
 import tkinter as tk
 #from tkinter import ttk
-HEIGHT = 800
-WIDTH = 800
+HEIGHT = 1000
+WIDTH = 1000
+
+def on_func():
+	print("Status: Active")
+	status.config(text="Status: Active")
+	status.config(fg="green")
+def off_func():
+	print("Status: Inactive")
+	status.config(text="Status: Inactive")
+	status.config(fg="red")
+
 root = tk.Tk()
 root.title("XXX  PEACE KEEPER  XXX")
-#photo = tk.PhotoImage(file="camo.PPM")
-
-# l = tk.Label(root, image=photo)
-# l.pack()
-# label = tk.Label(frame,text="This is Label",bg="yellow")
-# label.place(relx=0.3,rely=0,relwidth=0.45,relheight=0.25)
 
 canvas = tk.Canvas(root,height=HEIGHT,width=WIDTH)
 canvas.pack()
 
-frame = tk.Frame(root,bg="#80c1ff")
-frame.place(relx=0.5,rely=0.1,relwidth=0.75,relheight=0.1,anchor="n")
+background_image = tk.PhotoImage(file="camo.PPM")
+background_label = tk.Label(root, image=background_image)
+background_label.place(relwidth=1, relheight=1)
 
-entry = tk.Entry(frame ,font=40)
-entry.place(relwidth=0.65,relheight=1)
 
-button = tk.Button(frame, text = "  ON  ", bg="red", fg="green",highlightbackground="#80c1ff",font=40)
-button.place(relx=0.7,relwidth=0.35,relheight=1)
+live_frame = tk.Frame(root,bg="#091722",bd=5)
+live_frame.place(relx=0.05,rely=0.05,relwidth=0.6,relheight=0.5)
+
+live_label = tk.Label(live_frame)
+live_label.place(relwidth=1,relheight=1)
+
+button_frame = tk.Frame(root,bg="#091722",bd=5)
+button_frame.place(relx=0.7,rely=0.05,relwidth=0.25,relheight=0.1)
+
+on = tk.Button(button_frame,text = " ON ", bg="#54625B", fg="white",highlightbackground="#54625B",command=lambda:on_func())
+on.place(relwidth=0.5,relheight=1)
+off = tk.Button(button_frame,text = " OFF ", bg="#54625B", fg="white",highlightbackground="#54625B",command=lambda:off_func())
+off.place(relx=0.5,relwidth=0.5,relheight=1)
+
+status = tk.Label(root,text="Status: Active ",font=("Courier", 20),bg="#091823",fg="green")
+status.place(relx=0.7,rely=0.35,relwidth=0.25,relheight=0.1)
+
+
+
 
 
 root.mainloop()
